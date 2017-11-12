324d8fb2e32b7ebb5ca8d9ca6322d08fc3f31cf0
Khary Mendez <kharyam@gmail.com>
2017-11-12 10:22:07 -0500

    Added EAP 6 SCAP content

--- eclipse-che/install.sh
+++ eclipse-che/install.sh
@@ -3,6 +3,7 @@ export OPENSHIFT_ENDPOINT=https://ocp:8443
 export OPENSHIFT_TOKEN=$(oc whoami -t)
 export OPENSHIFT_NAMESPACE_URL=che-eclipse-che.apps.192.168.124.215.nip.io
 export OPENSHIFT_FLAVOR=ocp
+export CHE_IMAGE_TAG=5.17.0-centos 
 
 DEPLOY_SCRIPT_URL=https://raw.githubusercontent.com/eclipse/che/master/dockerfiles/init/modules/openshift/files/scripts/deploy_che.sh
 WAIT_SCRIPT_URL=https://raw.githubusercontent.com/eclipse/che/master/dockerfiles/init/modules/openshift/files/scripts/wait_until_che_is_available.sh
